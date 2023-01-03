2f934ed42cd225f20407385f96b9389491b04abf
CHOI97 <chlwjrh@gmail.com>
2023-01-04 05:24:57 +0900

    mac test

---
+++ mac_test/app/src/main/java/com/example/mac_test/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.mac_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
