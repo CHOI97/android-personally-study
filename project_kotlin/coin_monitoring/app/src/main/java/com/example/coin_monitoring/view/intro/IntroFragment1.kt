562e26447a6578366c0ca5f743979058c7000596
CHOI97 <chlwjrh@gmail.com>
2023-03-14 14:48:14 +0900

    [feat] fragment nav action

--- project_kotlin/coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/intro/IntroFragment1.kt
+++ project_kotlin/coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/intro/IntroFragment1.kt
@@ -5,8 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.coin_monitoring.R
+import com.example.coin_monitoring.databinding.FragmentIntro1Binding
+
+// findViewById -> viewBinding
+
 class IntroFragment1 : Fragment() {
+
+    private var _binding: FragmentIntro1Binding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +26,21 @@ class IntroFragment1 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro1, container, false)
+        _binding = FragmentIntro1Binding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextBtn.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_introFragment1_to_introFragment2)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
