715070ab73622adb0665a8f5a84c6e881a749b07
CHOI97 <chlwjrh@gmail.com>
2023-03-14 14:56:28 +0900

    [feat] Home | SelectActivity

--- project_kotlin/coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/intro/IntroFragment2.kt
+++ project_kotlin/coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/intro/IntroFragment2.kt
@@ -1,13 +1,18 @@
 package com.example.coin_monitoring.view.intro
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.coin_monitoring.R
+import com.example.coin_monitoring.databinding.FragmentIntro2Binding
+import com.example.coin_monitoring.view.SelectActivity
+
 class IntroFragment2 : Fragment() {
 
+    private var _binding : FragmentIntro2Binding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,7 +22,21 @@ class IntroFragment2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro2, container, false)
+        _binding = FragmentIntro2Binding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextBtn.setOnClickListener {
+            val intent = Intent(requireContext(), SelectActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
