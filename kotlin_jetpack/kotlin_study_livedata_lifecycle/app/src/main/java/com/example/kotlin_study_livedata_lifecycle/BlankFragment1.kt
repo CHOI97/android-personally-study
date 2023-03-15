c4a9d92f3e93ee93fc753c955d7db7fdf9704996
CHOI97 <chlwjrh@gmail.com>
2023-03-16 04:59:05 +0900

    fragment livedata viewLifecycleOwner

--- kotlin_jetpack/kotlin_study_livedata_lifecycle/app/src/main/java/com/example/kotlin_study_livedata_lifecycle/BlankFragment1.kt
+++ kotlin_jetpack/kotlin_study_livedata_lifecycle/app/src/main/java/com/example/kotlin_study_livedata_lifecycle/BlankFragment1.kt
@@ -6,12 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.kotlin_study_livedata_lifecycle.databinding.FragmentBlank1Binding
 
 class BlankFragment1 : Fragment() {
 
     // ViewBinding + LiveData
 
     private val TAG = "BlankFragment1"
+
+    private var _binding: FragmentBlank1Binding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: BlankViewModel1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,8 +29,21 @@ class BlankFragment1 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank1, container, false)
+        _binding = FragmentBlank1Binding.inflate(inflater,container,false)
+        val view = binding.root
+
+        viewModel = ViewModelProvider(this).get(BlankViewModel1::class.java)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btn1.setOnClickListener {
+            viewModel.plusCountValue()
+        }
+        viewModel.liveCount.observe(viewLifecycleOwner, Observer {
+            binding.text1.text = it.toString()
+        })
     }
 
     override fun onDestroyView() {
