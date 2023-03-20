9ef79b233e1bec9e92decb1094975fb51d86bb30
CHOI97 <chlwjrh@gmail.com>
2023-03-20 14:53:28 +0900

    디렉토리 변경

--- project_kotlin/coin_monitoring/app/src/main/java/com/example/coin_monitoring/dataModel/CurrentPriceResult.kt
+++
@@ -1,6 +0,0 @@
-package com.example.coin_monitoring.dataModel
-
-data class CurrentPriceResult (
-    val coinName: String,
-    val coinInfo: CurrentPrice
-)
--- project_kotlin/coin_monitoring/app/src/main/java/com/example/coin_monitoring/network/model/CurrentPriceList.kt
+++
@@ -1,6 +0,0 @@
-package com.example.coin_monitoring.network.model
-
-data class CurrentPriceList(
-    val status: String,
-    val data: Map<String, Any>
-)
