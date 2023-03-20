d2d866e213012e8ae92cc5027279bc3c65a8af31
CHOI97 <chlwjrh@gmail.com>
2023-03-20 13:23:28 +0900

    ROOM DataBase Coroutine

--- kotlin_jetpack/kotlin_study_room/app/src/main/java/com/example/kotlin_study_room/MainActivity.kt
+++ kotlin_jetpack/kotlin_study_room/app/src/main/java/com/example/kotlin_study_room/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.kotlin_study_room
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,10 +19,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val db = TextDatabase.getDatabase(this)
 
-        CoroutineScope(Dispatchers.IO).launch{
-            db.textDao().insert(TextEntity(0,"asdf"))
-            Log.d("MAINACTIVITY",db.textDao().getAllData().toString())
+        val inputArea = findViewById<EditText>(R.id.textInputArea)
+        val insertBtn = findViewById<Button>(R.id.insert)
+        val getAllBtn = findViewById<Button>(R.id.getData)
+        val deleteBtn = findViewById<Button>(R.id.delete)
 
+        insertBtn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch{
+                db.textDao().insert(TextEntity(0,"asdf"))
+                Log.d("MAINACTIVITY",db.textDao().getAllData().toString())
+
+                inputArea.setText("")
+            }
+        }
+        getAllBtn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                Log.d("MAINACTIVITY", db.textDao().getAllData().toString())
+            }
+        }
+        deleteBtn.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                db.textDao().deleteAllData()
+            }
         }
     }
 }
