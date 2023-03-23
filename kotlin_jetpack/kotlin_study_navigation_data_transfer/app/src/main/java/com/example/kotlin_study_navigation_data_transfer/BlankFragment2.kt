6c0a8d0c9043f3bd1c882062e25dcd26d724225f
CHOI97 <chlwjrh@gmail.com>
2023-03-23 16:17:31 +0900

    Fragment navigation Bundle(Data Transfer)

---
+++ kotlin_jetpack/kotlin_study_navigation_data_transfer/app/src/main/java/com/example/kotlin_study_navigation_data_transfer/BlankFragment2.kt
@@ -0,0 +1,28 @@
+package com.example.kotlin_study_navigation_data_transfer
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class BlankFragment2 : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_blank2, container, false)
+        val getString = arguments?.getString("key")
+        Log.d("BlankFragment2",getString.toString())
+        return view
+    }
+
+}
