0cd740adc8286ce2f3484f6f2be59f85880402ef
CHOI97 <chlwjrh@gmail.com>
2023-03-23 15:16:30 +0900

    Fragment Navigation action

---
+++ kotlin_jetpack/kotlin_study_navigation_graph/app/src/main/java/com/example/kotlin_study_navigation_graph/BlankFragment2.kt
@@ -0,0 +1,30 @@
+package com.example.kotlin_study_navigation_graph
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+class BlankFragment2 : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_blank2, container, false)
+        view.findViewById<Button>(R.id.btn2).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_blankFragment2_to_blankFragment1)
+        }
+        return view
+    }
+
+}
---
+++ kotlin_jetpack/kotlin_study_navigation_graph/app/src/main/java/com/example/kotlin_study_navigation_graph/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_study_navigation_graph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
