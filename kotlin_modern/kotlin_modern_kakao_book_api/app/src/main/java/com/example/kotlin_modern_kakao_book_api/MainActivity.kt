5832e115a9fd8a9169e576437cd1a46d6b75df5d
CHOI97 <chlwjrh@gmail.com>
2023-03-28 21:38:48 +0900

    init book search , kakao API

---
+++ kotlin_modern/kotlin_modern_kakao_book_api/app/src/main/java/com/example/kotlin_modern_kakao_book_api/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_modern_kakao_book_api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
