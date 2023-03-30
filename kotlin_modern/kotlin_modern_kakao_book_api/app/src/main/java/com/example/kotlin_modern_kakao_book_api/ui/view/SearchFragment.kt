8d6062580fd9eb90d010bc00c9fb5845a58fd363
CHOI97 <chlwjrh@gmail.com>
2023-03-30 15:31:16 +0900

    add Book info  WebView | safeArgs, Parcelable

--- kotlin_modern/kotlin_modern_kakao_book_api/app/src/main/java/com/example/kotlin_modern_kakao_book_api/ui/view/SearchFragment.kt
+++ kotlin_modern/kotlin_modern_kakao_book_api/app/src/main/java/com/example/kotlin_modern_kakao_book_api/ui/view/SearchFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlin_modern_kakao_book_api.databinding.FragmentSearchBinding
@@ -57,6 +58,10 @@ class SearchFragment : Fragment() {
                 )
             )
             adapter = bookSearchAdapter
+        }
+        bookSearchAdapter.setOnItemClickListener {
+            val action = SearchFragmentDirections.actionFragmentSearchToFragmentBook(it)
+            findNavController().navigate(action)
         }
     }
 
