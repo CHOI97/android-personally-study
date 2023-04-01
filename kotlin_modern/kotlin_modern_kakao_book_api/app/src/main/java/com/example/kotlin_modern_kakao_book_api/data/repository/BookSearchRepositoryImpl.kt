ffa7e08879c4936e9b6dcda77f624411d1d4d551
CHOI97 <chlwjrh@gmail.com>
2023-04-01 18:05:44 +0900

    Favorite Books Recylcerview, Room, Swipe, SimpleCallback

--- kotlin_modern/kotlin_modern_kakao_book_api/app/src/main/java/com/example/kotlin_modern_kakao_book_api/data/repository/BookSearchRepositoryImpl.kt
+++ kotlin_modern/kotlin_modern_kakao_book_api/app/src/main/java/com/example/kotlin_modern_kakao_book_api/data/repository/BookSearchRepositoryImpl.kt
@@ -1,10 +1,16 @@
 package com.example.kotlin_modern_kakao_book_api.data.repository
 
+import androidx.lifecycle.LiveData
 import com.example.kotlin_modern_kakao_book_api.data.api.RetrofitInstance.api
+import com.example.kotlin_modern_kakao_book_api.data.db.BookSearchDatabase
+import com.example.kotlin_modern_kakao_book_api.data.model.Book
 import com.example.kotlin_modern_kakao_book_api.data.model.SearchResponse
 import retrofit2.Response
 
-class BookSearchRepositoryImpl : BookSearchRepository {
+class BookSearchRepositoryImpl(
+    private val db: BookSearchDatabase
+) : BookSearchRepository {
+
     override suspend fun searchBooks(
         query: String,
         sort: String,
@@ -12,5 +18,17 @@ class BookSearchRepositoryImpl : BookSearchRepository {
         size: Int
     ): Response<SearchResponse> {
         return api.searchBooks(query, sort, page, size)
+    }
+
+    override suspend fun insertBook(book: Book) {
+        db.bookSearchDao().insertBook(book)
+    }
+
+    override suspend fun deleteBook(book: Book) {
+        db.bookSearchDao().deleteBook(book)
+    }
+
+    override fun getFavoriteBooks(): LiveData<List<Book>> {
+        return db.bookSearchDao().getFavoriteBooks()
     }
 }
