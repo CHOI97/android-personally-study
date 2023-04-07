59cdf2da2aad2791488277ee57d5d545612b7597
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-04-07 15:56:40 +0900

    Hilt 의존성 작성 AppModule, RepositoryModule

---
+++ kotlin_modern/kotlin_modern_kakao_book_api/app/src/main/java/com/example/kotlin_modern_kakao_book_api/BookSearchApplication.kt
@@ -0,0 +1,8 @@
+package com.example.kotlin_modern_kakao_book_api
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BookSearchApplication : Application() {
+}
