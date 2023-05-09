ceaabd2dbe1df6b85f1174286e9eea75f2f42076
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-05-09 22:25:30 +0900

    커스텀 다이얼로그

---
+++ kotlin_study_custom_dialog/app/src/main/java/com/example/kotlin_study_custom_dialog/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.kotlin_study_custom_dialog
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.kotlin_study_custom_dialog.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
