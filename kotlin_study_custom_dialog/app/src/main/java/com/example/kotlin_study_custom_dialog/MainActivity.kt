a2c7a482e77c926ae0441ecdeb028bb1fbccede5
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-05-10 14:56:10 +0900

    기본 다이얼로그

--- kotlin_study_custom_dialog/app/src/main/java/com/example/kotlin_study_custom_dialog/MainActivity.kt
+++ kotlin_study_custom_dialog/app/src/main/java/com/example/kotlin_study_custom_dialog/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import com.example.kotlin_study_custom_dialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 기본 다이얼로그
+        binding.dialog1.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Title")
+                .setMessage("기본 다이얼로그")
+                .create()
+                .show()
+        }
 
     }
 }
