0a87e9d7e551db1e1a69961b5f53a5a56f83a7f5
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-05-23 15:48:59 +0900

    findviewById version

---
+++ kotlin_jetpack/kotlin_study_2way _databinding/kotlin_study_databinding_bindingAdapter/app/src/main/java/com/example/kotlin_study_databinding_bindingadapter/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.kotlin_study_databinding_bindingadapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+// 양방향 데이터 결합( Two - Way Binding)
+// BindingAdapter -> View 에 속성을 내 마음대로 수정
+// 1. 기존에 양방향 데이터 결합 / BindingAdapter 사용하지 않고 제작
+// 2. 양방향 데티터 결합 / BindingAdapter를 사용해서 제작
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var age = 0
+        val ageArea = findViewById<TextView>(R.id.ageArea)
+        val imgArea = findViewById<ImageView>(R.id.imgArea)
+        val plus = findViewById<Button>(R.id.plus)
+
+        plus.setOnClickListener {
+            age ++
+            ageArea.text = age.toString()
+            if(age>20){
+                imgArea.setImageResource(R.drawable.choi)
+            }
+        }
+    }
+}
