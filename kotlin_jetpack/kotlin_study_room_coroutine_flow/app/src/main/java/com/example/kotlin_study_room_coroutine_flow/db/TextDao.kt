dd81a64a857088902d90bd1139c72fb1b13cf376
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-05-28 14:41:44 +0900

    Coroutine + Flow Observer

--- kotlin_jetpack/kotlin_study_room_coroutine_flow/app/src/main/java/com/example/kotlin_study_room_coroutine_flow/db/TextDao.kt
+++ kotlin_jetpack/kotlin_study_room_coroutine_flow/app/src/main/java/com/example/kotlin_study_room_coroutine_flow/db/TextDao.kt
@@ -4,13 +4,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TextDao {
     @Query("SELECT * FROM text_table")
     fun getAllData(): List<TextEntity>
+
+    @Query("SELECT * FROM text_table")
+    fun getAllDataFlow(): Flow<List<TextEntity>>
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(text: TextEntity)
+
     @Query("DELETE FROM text_table")
     fun deleteAllData()
 }
