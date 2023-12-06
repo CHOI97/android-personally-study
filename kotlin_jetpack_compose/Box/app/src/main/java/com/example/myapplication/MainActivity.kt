ebc2b380f7f6e3a56ad4a514501303aed6ee4d88
CHOI97 <chlwjrh@gmail.com>
2023-12-06 16:34:17 +0900

    jetpack compose box

---
+++ kotlin_jetpack_compose/Box/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Box(modifier = Modifier.background(color = Color.Green)
+                .fillMaxSize()){
+                Text("Hello")
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.BottomEnd,
+                ){
+                    Text("1234~~~!!!")
+                }
+            }
+        }
+    }
+}
