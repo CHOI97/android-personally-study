b630f22131de2c18ea89293cf9203c312dd0fbd4
CHOI97 <chlwjrh@gmail.com>
2023-12-13 00:49:58 +0900

    jetpack compose State, ViewModel

---
+++ kotlin_jetpack_compose/ViewModel/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val viewModel = viewModel<MainViewModel>()
+            val data = remember{
+                mutableStateOf("Hello")
+            }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    viewModel.data.value,
+                    fontSize = 30.sp
+                )
+                Button(onClick = {
+                    viewModel.changeValue()
+                }) {
+                    Text(text = "변경")
+                }
+
+            }
+        }
+    }
+}
+
+class MainViewModel: ViewModel(){
+    private val _data = mutableStateOf("Hello")
+    val data: State<String> = _data
+
+    fun changeValue() {
+        _data.value = "World"
+    }
+}
